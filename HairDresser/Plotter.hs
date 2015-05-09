fb143af00d7a8dee0dc327ea969fc72dcb6f498d
littorio <littorio@gmail.com>
2015-05-09 11:18:37 +0200

    A method for plotting a centered angle was extracted.

--- HairDresser/Plotter.hs
+++ HairDresser/Plotter.hs
@@ -46,22 +46,30 @@ drawArcBy3Points (x1 :+ y1) (xc :+ yc) (x4 :+ y4) = do
             let y3 = yc + by - k2*bx
             plotOneBezier (Bezier ((x1:+y1), (x2:+y2), (x3:+y3), (x4:+y4)))
 
-drawArcByCenterRadiusAndAngles2 :: Point -> Angle -> Angle -> Double -> Draw ()
-drawArcByCenterRadiusAndAngles2 (xc :+ yc) startAngle endAngle r = do
+bezierControlPointsForCenteredCircle :: Double -> Angle -> (Point, Point)
+bezierControlPointsForCenteredCircle r arcAngle = ((x2 :+ y2), (x3 :+ y3))
+    where
+      a = toRadian arcAngle / 2
+-- This one is suggestedby internet, but it is less pleasant for my eye    let k = 0.5522847498
+      k = 0.6522847498
+      x4 = r * cos(a)
+      y4 = r * sin (a)
+      x1 = x4
+      y1 = -y4
+      f = k * (tan a)
+      x2 = x1 + f * y4
+      y2 = y1 + f * x4
+      x3 = x2
+      y3 = -y2
+
+
+drawArcByCenterRadiusAndAngles :: Point -> Angle -> Angle -> Double -> Draw ()
+drawArcByCenterRadiusAndAngles (xc :+ yc) startAngle endAngle r = do
             let a1 = toRadian startAngle
             let a2 = toRadian endAngle
+            let ((x2 :+ y2), (x3 :+ y3)) = bezierControlPointsForCenteredCircle r $ Radian (a2 - a1)
             let a = (a2 - a1) / 2
--- This one is suggestedby internet, but it is less pleasant for my eye    let k = 0.5522847498
-            let k = 0.6522847498
-            let x4 = r * cos(a)
-            let y4 = r * sin (a)
-            let x1 = x4
-            let y1 = -y4
-            let f = k * (tan a)
-            let x2 = x1 + f * y4
-            let y2 = y1 + f * x4
-            let x3 = x2
-            let y3 = -y2
+
             let ar = a + a1
             let cos_ar = cos(ar)
             let sin_ar = sin(ar)
@@ -76,41 +84,26 @@ drawArcByCenterRadiusAndAngles2 (xc :+ yc) startAngle endAngle r = do
             let yf4 = yc + r * (sin a2)
             plotOneBezier (Bezier ((xf1:+yf1), (xf2:+yf2), (xf3:+yf3), (xf4:+yf4)))
 
-drawArcByCenterRadiusAndAngles :: Point -> Angle -> Angle -> Double -> Draw ()
-drawArcByCenterRadiusAndAngles (xc :+ yc) a b r = do
-            let aRadians = toRadian a
-            let bRadians = toRadian b
-            let xm = xc + r * sin (aRadians + bRadians / 2)
-            let ym = yc + r * cos (aRadians + bRadians / 2)
-            let x1 = xc + r * sin (aRadians)
-            let y1 = yc + r * cos (aRadians)
-            let x4 = xc + r * sin (aRadians + bRadians)
-            let y4 = yc + r * cos (aRadians + bRadians)
-            beginPath (x1 :+ y1)
-            lineto (xm:+ym)
-            lineto (x4:+y4)
-            strokePath
-            drawArcBy3Points (x1 :+ y1) (xm :+ ym) (x4 :+ y4)
 
 plotSamplePage :: Draw ()
 plotSamplePage = do  
                strokeColor blue
                setWidth 1
-               drawArcByCenterRadiusAndAngles2 (100 :+ 100) (Degree 0) (Degree 30) 70
+               drawArcByCenterRadiusAndAngles (100 :+ 100) (Degree 0) (Degree 30) 70
                --strokeColor green
-               drawArcByCenterRadiusAndAngles2 (100 :+ 100) (Degree 30) (Degree 60) 70
+               drawArcByCenterRadiusAndAngles (100 :+ 100) (Degree 30) (Degree 60) 70
                --strokeColor red
-               drawArcByCenterRadiusAndAngles2 (100 :+ 100) (Degree 60) (Degree 90) 70
+               drawArcByCenterRadiusAndAngles (100 :+ 100) (Degree 60) (Degree 90) 70
                --strokeColor black
-               drawArcByCenterRadiusAndAngles2 (100 :+ 100) (Degree 90) (Degree 120) 70
-               drawArcByCenterRadiusAndAngles2 (100 :+ 100) (Degree 120) (Degree 150) 70
-               drawArcByCenterRadiusAndAngles2 (100 :+ 100) (Degree 150) (Degree 180) 70
-               drawArcByCenterRadiusAndAngles2 (100 :+ 100) (Degree 180) (Degree 210) 70
-               drawArcByCenterRadiusAndAngles2 (100 :+ 100) (Degree 210) (Degree 240) 70
-               drawArcByCenterRadiusAndAngles2 (100 :+ 100) (Degree 240) (Degree 270) 70
-               drawArcByCenterRadiusAndAngles2 (100 :+ 100) (Degree 270) (Degree 300) 70
-               drawArcByCenterRadiusAndAngles2 (100 :+ 100) (Degree 300) (Degree 330) 70
-               drawArcByCenterRadiusAndAngles2 (100 :+ 100) (Degree 330) (Degree 360) 70
+               drawArcByCenterRadiusAndAngles (100 :+ 100) (Degree 90) (Degree 120) 70
+               drawArcByCenterRadiusAndAngles (100 :+ 100) (Degree 120) (Degree 150) 70
+               drawArcByCenterRadiusAndAngles (100 :+ 100) (Degree 150) (Degree 180) 70
+               drawArcByCenterRadiusAndAngles (100 :+ 100) (Degree 180) (Degree 210) 70
+               drawArcByCenterRadiusAndAngles (100 :+ 100) (Degree 210) (Degree 240) 70
+               drawArcByCenterRadiusAndAngles (100 :+ 100) (Degree 240) (Degree 270) 70
+               drawArcByCenterRadiusAndAngles (100 :+ 100) (Degree 270) (Degree 300) 70
+               drawArcByCenterRadiusAndAngles (100 :+ 100) (Degree 300) (Degree 330) 70
+               drawArcByCenterRadiusAndAngles (100 :+ 100) (Degree 330) (Degree 360) 70
 
                strokeColor red
                plotBeziers jinny
